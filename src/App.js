87f5a0f5d38266445f411257ae7a3a6193ca81e2
ChomynNicolas <chomynnicolas2@gmail.com>
2024-05-24 09:35:50 -0400

    practicando enrutamiento

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { Link, Route, Routes } from "react-router-dom";
+import { useParams } from "react-router";
+import "./App.css";
+
+const Home = () => {
+  return <div>Bienvenido</div>;
+};
+
+const Ver = (props) => {
+  const { num } = useParams();
+  
+  return( 
+  <>
+  {isNaN(num)?<div>La palabra es {num} </div>:<div>El numero es {num} </div>
+  }
+  </>  
+  );
+};
+
+const AñadirStyles = (props) => {
+  const { num, color, back } = useParams();
+  
+  return( 
+  <div style={ {backgroundColor: `${back}`,
+                color: `${color}`
+  }}>
+  {isNaN(num)?<div>La palabra es {num} </div>:<div>El numero es {num} </div>
+  }
+  </div>  
+  );
+};
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/:num" element={<Ver />} />
+        <Route path="/:num/:color/:back"
+        element={<AñadirStyles/>}/>
+        
+      </Routes>
     </div>
   );
 }
